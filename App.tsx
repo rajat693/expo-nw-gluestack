1b9f25d45e8dac8bb557ad75d7989c9815359250
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-04-19 11:06:52 +0530

    fix: alert icon issue

--- App.tsx
+++ App.tsx
@@ -1,12 +1,31 @@
-import "./global.css"
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from './components/ui';
+import "./global.css";
+import {
+  Text,
+  SafeAreaView,
+  Alert,
+  AlertIcon,
+  AlertText,
+  InfoIcon,
+  GluestackUIProvider,
+} from "./components/ui";
+import { Search } from "lucide-react-native";
 
 export default function App() {
   return (
-    <View >
-      <Text className='text-red-500'>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider>
+      <SafeAreaView className="justify-center items-center flex-1">
+        <Text className="text-red-500">
+          Open up App.tsx to start working on your app!
+        </Text>
+
+        <Alert className="mx-2.5" action="info" variant="solid">
+          <AlertIcon as={Search} className="mr-2" />
+          <AlertText>
+            We have updated our terms of service. Please review and accept to
+            continue using our service.
+          </AlertText>
+        </Alert>
+      </SafeAreaView>
+    </GluestackUIProvider>
   );
 }
