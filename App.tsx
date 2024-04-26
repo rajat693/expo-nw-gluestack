d64768fb4378696f4233972fc4bee412f6547aaa
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-04-26 15:54:44 +0530

    fix: updated linear-gradient

--- App.tsx
+++ App.tsx
@@ -1,90 +1,13 @@
-import "./global.css";
-import {
-  Text,
-  SafeAreaView,
-  GluestackUIProvider,
-  Button,
-  ButtonText,
-  Center,
-  CloseIcon,
-  Heading,
-  Icon,
-  Modal,
-  ModalBackdrop,
-  ModalBody,
-  ModalCloseButton,
-  ModalContent,
-  ModalFooter,
-  ModalHeader,
-} from "./components/ui";
-import { useState, useRef } from "react";
+import React from "react";
+import { Image, GluestackUIProvider } from "./components/ui";
 
-function Example() {
-  const [showModal, setShowModal] = useState(false);
-  console.log(showModal);
-  const ref = useRef(null);
-  return (
-    <Center className='flex-1 bg-red-200'>
-      <Button onPress={() => setShowModal(true)} ref={ref}>
-        <ButtonText>Show Modal</ButtonText>
-      </Button>
-      <Modal
-        isOpen={showModal}
-        onClose={() => {
-          setShowModal(false);
-        }}
-        finalFocusRef={ref}
-      >
-        <ModalBackdrop className='bg-green-200' />
-        <ModalContent>
-          <ModalHeader>
-            <Heading size="lg">Engage with Modals</Heading>
-            <ModalCloseButton>
-              <Icon as={CloseIcon} />
-            </ModalCloseButton>
-          </ModalHeader>
-          <ModalBody>
-            <Text>
-              Elevate user interactions with our versatile modals. Seamlessly
-              integrate notifications, forms, and media displays. Make an impact
-              effortlessly.
-            </Text>
-          </ModalBody>
-          <ModalFooter>
-            <Button
-              variant="outline"
-              size="sm"
-              action="secondary"
-              className="mr-3"
-              onPress={() => {
-                setShowModal(false);
-              }}
-            >
-              <ButtonText>Cancel</ButtonText>
-            </Button>
-            <Button
-              size="sm"
-              action="positive"
-              className="border-0"
-              onPress={() => {
-                setShowModal(false);
-              }}
-            >
-              <ButtonText>Explore</ButtonText>
-            </Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
-    </Center>
-  );
-}
-
-export default function App() {
+export const App = () => {
   return (
     <GluestackUIProvider>
-      <SafeAreaView className="flex-1">
-        <Example />
-      </SafeAreaView>
+      <Image
+        source={require("./assets/gluestack-ui-logo.png")}
+        alt="gluestack-logo"
+      />
     </GluestackUIProvider>
   );
-}
+};
