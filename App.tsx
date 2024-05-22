d27d92aa911453b1f0c95ea36a15dda3abc966d5
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-05-22 18:11:53 +0530

    fix: expo version

--- App.tsx
+++ App.tsx
@@ -1,8 +1,6 @@
 import "./global.css";
 import React from "react";
 import {
-  Heading,
-  Text,
   SafeAreaView,
   GluestackUIProvider,
   AddIcon,
@@ -18,45 +16,60 @@ import {
 } from "./components/ui";
 import { PuzzleIcon, PaintBucket } from "lucide-react-native";
 
+const TestingMenu = () => {
+  return (
+    <Box className="h-screen w-screen justify-center items-center">
+      <Menu
+        placement="top"
+        trigger={({ ...triggerProps }) => {
+          return (
+            <Button {...triggerProps}>
+              <ButtonText>Menu</ButtonText>
+            </Button>
+          );
+        }}
+      >
+        <MenuItem key="Community" textValue="Community">
+          <Icon as={GlobeIcon} size="sm" mr="$2" />
+          <MenuItemLabel size="sm">Community</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Plugins" textValue="Plugins">
+          {/* PuzzleIcon is imported from 'lucide-react-native' */}
+          <Icon as={PuzzleIcon} size="sm" mr="$2" />
+          <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Theme" textValue="Theme">
+          {/* PaintBucket is imported from 'lucide-react-native' */}
+          <Icon as={PaintBucket} size="sm" mr="$2" />
+          <MenuItemLabel size="sm">Theme</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Settings" textValue="Settings">
+          <Icon as={SettingsIcon} size="sm" mr="$2" />
+          <MenuItemLabel size="sm">Settings</MenuItemLabel>
+        </MenuItem>
+        <MenuItem key="Add account" textValue="Add account">
+          <Icon as={AddIcon} size="sm" mr="$2" />
+          <MenuItemLabel size="sm">Add account</MenuItemLabel>
+        </MenuItem>
+      </Menu>
+    </Box>
+  );
+};
+
 export default function App() {
+  const [colorMode, setColorMode] = React.useState<"dark" | "light">("dark");
   return (
     <SafeAreaView className="flex-1 justify-center items-center">
-      <GluestackUIProvider mode="light">
-        <Box className="h-screen w-screen justify-center items-center">
-          <Menu
-            placement="top"
-            trigger={({ ...triggerProps }) => {
-              return (
-                <Button {...triggerProps}>
-                  <ButtonText>Menu</ButtonText>
-                </Button>
-              );
-            }}
-          >
-            <MenuItem key="Community" textValue="Community">
-              <Icon as={GlobeIcon} size="sm" mr="$2" />
-              <MenuItemLabel size="sm">Community</MenuItemLabel>
-            </MenuItem>
-            <MenuItem key="Plugins" textValue="Plugins">
-              {/* PuzzleIcon is imported from 'lucide-react-native' */}
-              <Icon as={PuzzleIcon} size="sm" mr="$2" />
-              <MenuItemLabel size="sm">Plugins</MenuItemLabel>
-            </MenuItem>
-            <MenuItem key="Theme" textValue="Theme">
-              {/* PaintBucket is imported from 'lucide-react-native' */}
-              <Icon as={PaintBucket} size="sm" mr="$2" />
-              <MenuItemLabel size="sm">Theme</MenuItemLabel>
-            </MenuItem>
-            <MenuItem key="Settings" textValue="Settings">
-              <Icon as={SettingsIcon} size="sm" mr="$2" />
-              <MenuItemLabel size="sm">Settings</MenuItemLabel>
-            </MenuItem>
-            <MenuItem key="Add account" textValue="Add account">
-              <Icon as={AddIcon} size="sm" mr="$2" />
-              <MenuItemLabel size="sm">Add account</MenuItemLabel>
-            </MenuItem>
-          </Menu>
-        </Box>
+      <GluestackUIProvider mode={colorMode}>
+        <Button
+          onPress={() =>
+            setColorMode((prev) => (prev === "light" ? "dark" : "light"))
+          }
+          className="mt-10 bg-error-500"
+        >
+          <ButtonText>{colorMode}</ButtonText>
+        </Button>
+        <TestingMenu />
       </GluestackUIProvider>
     </SafeAreaView>
   );
