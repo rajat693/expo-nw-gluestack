8308286818f4a2fca6465858a34d9fa5ac8a458a
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-06-06 12:39:34 +0530

    feat: updated all the components

--- App.tsx
+++ App.tsx
@@ -3,25 +3,11 @@ import React from "react";
 import {
   SafeAreaView,
   GluestackUIProvider,
-  Button,
-  ButtonText,
-  Popover,
-  PopoverBackdrop,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverFooter,
-  PopoverHeader,
-  Text,
-  Heading,
-  CloseIcon,
-  Icon,
-  PopoverArrow,
 } from "./components/ui";
 
 export default function App() {
   const [colorMode, setColorMode] = React.useState<"dark" | "light">("light");
-  
+
   return (
     <GluestackUIProvider mode={colorMode}>
       <SafeAreaView className="flex-1 justify-center items-center">
