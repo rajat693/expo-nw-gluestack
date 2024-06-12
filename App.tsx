47fa48111a4cfa737fdfb798b3b5b4a5984c9bf8
Rajat Chaudhary <rajatchaudhary@Rajats-MacBook-Air.local>
2024-06-12 15:30:55 +0530

    fix: working on menu position issue in android

--- App.tsx
+++ App.tsx
@@ -1,59 +1,67 @@
 import "./global.css";
-import React from "react";
+import React, { useState } from "react";
 import {
-  SafeAreaView,
   GluestackUIProvider,
   Text,
+  SafeAreaView,
+  View,
+  AddIcon,
   Button,
   ButtonText,
-  CloseIcon,
-  Heading,
+  MenuItem,
+  MenuItemLabel,
+  GlobeIcon,
   Icon,
-  Popover,
-  PopoverBackdrop,
-  PopoverBody,
-  PopoverCloseButton,
-  PopoverContent,
-  PopoverFooter,
-  PopoverHeader,
+  Menu,
+  SettingsIcon,
 } from "./components/ui";
+import { PaintBucket, PuzzleIcon } from "lucide-react-native";
 
 export default function App() {
-  const [colorMode, setColorMode] = React.useState<"dark" | "light">("light");
-  const [isOpen, setIsOpen] = React.useState(false);
-  const handleOpen = () => {
-    setIsOpen(true);
-  };
-  const handleClose = () => {
-    setIsOpen(false);
-  };
+  const [colorMode, setColorMode] = useState<"dark" | "light">("light");
 
   return (
     <GluestackUIProvider mode={colorMode}>
       <SafeAreaView className="flex-1 justify-center items-center">
-        {/* <Popover
-          isOpen={isOpen}
-          onClose={handleClose}
-          onOpen={handleOpen}
-          placement="bottom"
-          size="md"
-          trigger={(triggerProps) => {
+        {/* <View className="h-40 w-40 bg-red-200">
+          <Text className="dark:text-green-600 text-blue-600">Simple Text</Text>
+        </View> */}
+
+        <Menu
+          size="lg"
+          // offset={5}
+          placement="top"
+          trigger={({ ...triggerProps }) => {
             return (
               <Button {...triggerProps}>
-                <ButtonText>Open dropdown</ButtonText>
+                <ButtonText>Menu</ButtonText>
               </Button>
             );
           }}
         >
-          <PopoverBackdrop />
-          <PopoverContent>
-            <Text>Click outside to close</Text>
-          </PopoverContent>
-        </Popover> */}
-        <Button>
-          <ButtonText>button</ButtonText>
-        </Button>
-        <Text size="2xl">hello world</Text>
+          <MenuItem key="Community" textValue="Community">
+            <Icon as={GlobeIcon} size="sm" className="mr-2" />
+            <MenuItemLabel size="sm">Community</MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Plugins" textValue="Plugins">
+            {/* PuzzleIcon is imported from 'lucide-react-native' */}
+            <Icon as={PuzzleIcon} size="sm" className="mr-2" />
+            <MenuItemLabel size="sm">Plugins</MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Theme" textValue="Theme">
+            {/* PaintBucket is imported from 'lucide-react-native' */}
+            <Icon as={PaintBucket} size="sm" className="mr-2" />
+            <MenuItemLabel size="sm">Theme</MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Settings" textValue="Settings">
+            <Icon as={SettingsIcon} size="sm" className="mr-2" />
+            <MenuItemLabel size="sm">Settings</MenuItemLabel>
+          </MenuItem>
+          <MenuItem key="Add account" textValue="Add account">
+            <Icon as={AddIcon} size="sm" className="mr-2" />
+            <MenuItemLabel size="sm">Add account</MenuItemLabel>
+          </MenuItem>
+        </Menu>
       </SafeAreaView>
     </GluestackUIProvider>
   );
--- components/ui/menu/index.tsx
+++ components/ui/menu/index.tsx
@@ -1,64 +1,64 @@
-'use client';
-import React from 'react';
-import { createMenu } from '@gluestack-ui/menu';
-import { tva } from '@gluestack-ui/nativewind-utils/tva';
-import { cssInterop } from 'nativewind';
-import { Pressable, Text, Platform } from 'react-native';
-import { Motion, AnimatePresence } from '@legendapp/motion';
-import type { VariantProps } from '@gluestack-ui/nativewind-utils';
-import { withStates } from '@gluestack-ui/nativewind-utils/withStates';
+"use client";
+import React from "react";
+import { createMenu } from "./src";
+import { tva } from "@gluestack-ui/nativewind-utils/tva";
+import { cssInterop } from "nativewind";
+import { Pressable, Text, Platform } from "react-native";
+import { Motion, AnimatePresence } from "@legendapp/motion";
+import type { VariantProps } from "@gluestack-ui/nativewind-utils";
+import { withStates } from "@gluestack-ui/nativewind-utils/withStates";
 
 const menuStyle = tva({
-  base: 'rounded bg-background-0 border border-outline-200 p-1 shadow-soft-3',
+  base: "rounded bg-background-0 border border-outline-200 p-1 shadow-soft-3",
 });
 
 const menuItemStyle = tva({
-  base: 'min-w-[200px] p-3 flex-row items-center data-[hover=true]:bg-background-50 data-[active=true]:bg-background-100 data-[focus=true]:bg-background-50 data-[focus=true]:web:outline-none data-[focus=true]:web:outline-0 data-[disabled=true]:opacity-40 data-[disabled=true]:web:cursor-not-allowed data-[focus-visible=true]:web:outline-2 data-[focus-visible=true]:web:outline-primary-700 data-[focus-visible=true]:web:outline data-[focus-visible=true]:web:cursor-pointer data-[disabled=true]:data-[focus=true]:bg-transparent rounded-sm',
+  base: "min-w-[200px] p-3 flex-row items-center data-[hover=true]:bg-background-50 data-[active=true]:bg-background-100 data-[focus=true]:bg-background-50 data-[focus=true]:web:outline-none data-[focus=true]:web:outline-0 data-[disabled=true]:opacity-40 data-[disabled=true]:web:cursor-not-allowed data-[focus-visible=true]:web:outline-2 data-[focus-visible=true]:web:outline-primary-700 data-[focus-visible=true]:web:outline data-[focus-visible=true]:web:cursor-pointer data-[disabled=true]:data-[focus=true]:bg-transparent rounded-sm",
 });
 
 const menuBackdropStyle = tva({
-  base: 'absolute top-0 bottom-0 left-0 right-0 web:cursor-default',
+  base: "absolute top-0 bottom-0 left-0 right-0 web:cursor-default",
   // add this classnames if you want to give background color to backdrop
   // opacity-50 bg-background-500,
 });
 
 const menuItemLabelStyle = tva({
-  base: 'text-typography-700 font-normal font-body',
+  base: "text-typography-700 font-normal font-body",
 
   variants: {
     isTruncated: {
-      true: 'web:truncate',
+      true: "web:truncate",
     },
     bold: {
-      true: 'font-bold',
+      true: "font-bold",
     },
     underline: {
-      true: 'underline',
+      true: "underline",
     },
     strikeThrough: {
-      true: 'line-through',
+      true: "line-through",
     },
     size: {
-      '2xs': 'text-2xs',
-      'xs': 'text-xs',
-      'sm': 'text-sm',
-      'md': 'text-base',
-      'lg': 'text-lg',
-      'xl': 'text-xl',
-      '2xl': 'text-2xl',
-      '3xl': 'text-3xl',
-      '4xl': 'text-4xl',
-      '5xl': 'text-5xl',
-      '6xl': 'text-6xl',
+      "2xs": "text-2xs",
+      xs: "text-xs",
+      sm: "text-sm",
+      md: "text-base",
+      lg: "text-lg",
+      xl: "text-xl",
+      "2xl": "text-2xl",
+      "3xl": "text-3xl",
+      "4xl": "text-4xl",
+      "5xl": "text-5xl",
+      "6xl": "text-6xl",
     },
     sub: {
-      true: 'text-xs',
+      true: "text-xs",
     },
     italic: {
-      true: 'italic',
+      true: "italic",
     },
     highlight: {
-      true: 'bg-yellow-500',
+      true: "bg-yellow-500",
     },
   },
 });
@@ -98,14 +98,14 @@ const Item = React.forwardRef(
 
 export const UIMenu = createMenu({
   Root: Motion.View,
-  Item: Platform.OS === 'web' ? Item : withStates(Item),
+  Item: Platform.OS === "web" ? Item : withStates(Item),
   Label: Text,
   Backdrop: BackdropPressable,
   AnimatePresence: AnimatePresence,
 });
 
-cssInterop(UIMenu, { className: 'style' });
-cssInterop(UIMenu.ItemLabel, { className: 'style' });
+cssInterop(UIMenu, { className: "style" });
+cssInterop(UIMenu.ItemLabel, { className: "style" });
 
 type IMenuProps = React.ComponentProps<typeof UIMenu> &
   VariantProps<typeof menuStyle>;
@@ -127,11 +127,11 @@ const Menu = React.forwardRef(
           opacity: 0,
         }}
         transition={{
-          type: 'spring',
+          type: "spring",
           damping: 18,
           stiffness: 250,
           opacity: {
-            type: 'timing',
+            type: "timing",
             duration: 200,
           },
         }}
@@ -154,7 +154,7 @@ const MenuItemLabel = React.forwardRef(
       bold,
       underline,
       strikeThrough,
-      size = 'md',
+      size = "md",
       sub,
       italic,
       highlight,
@@ -182,8 +182,8 @@ const MenuItemLabel = React.forwardRef(
   }
 );
 
-Menu.displayName = 'Menu';
-MenuItem.displayName = 'MenuItem';
-MenuItemLabel.displayName = 'MenuItemLabel';
+Menu.displayName = "Menu";
+MenuItem.displayName = "MenuItem";
+MenuItemLabel.displayName = "MenuItemLabel";
 
 export { Menu, MenuItem, MenuItemLabel };
